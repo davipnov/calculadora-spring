e4024503edb088d280d0bb5d198868ed4a8223b7
Davi P Novaes <davi_novaes@estudante.sesisenai.org.br>
2026-08-31 21:48:08 -0300

    feat: Add OperationService

--- calculadora/src/main/java/com/novaes/calculadora/controller/OperationController.java
+++ calculadora/src/main/java/com/novaes/calculadora/controller/OperationController.java
@@ -1,9 +1,14 @@
 package com.novaes.calculadora.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OperationController {
+
+    @PostMapping ("calculadora")
+    public {
+    }
 
 
 }
---
+++ calculadora/src/main/java/com/novaes/calculadora/service/OperationService.java
@@ -0,0 +1,9 @@
+package com.novaes.calculadora.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OperationService {
+
+    
+}
