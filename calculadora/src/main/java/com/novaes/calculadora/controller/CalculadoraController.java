17fabcfe9f1beb5679ffe91043d981d3a47ddf0f
Davi P Novaes <davi_novaes@estudante.sesisenai.org.br>
2026-09-03 21:09:43 -0300

    feat: Update CalculadoraController

--- calculadora/src/main/java/com/novaes/calculadora/controller/CalculadoraController.java
+++ calculadora/src/main/java/com/novaes/calculadora/controller/CalculadoraController.java
@@ -1,14 +1,44 @@
 package com.novaes.calculadora.controller;
 
+import com.novaes.calculadora.dto.CalculadoraRequest;
+import com.novaes.calculadora.dto.CalculadoraResponse;
+import com.novaes.calculadora.service.CalculadoraService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class CalculadoraController {
 
-    @PostMapping ("calculadora")
-    public {
+    private final CalculadoraService calculadoraService;
 
+    public CalculadoraController(CalculadoraService calculadoraService) {
+        this.calculadoraService = calculadoraService;
+    }
+
+    @PostMapping("/somar")
+    public CalculadoraResponse somar(CalculadoraRequest request) {
+        double resultado = calculadoraService.somar(request.getNumero1(), request.getNumero2());
+        return new CalculadoraResponse(request.getNumero1(), request.getNumero2(), "soma", resultado);
+    }
+
+    @PostMapping("/subtrair")
+    public CalculadoraResponse subtrair(CalculadoraRequest request) {
+        double resultado = calculadoraService.subtrair(request.getNumero1(), request.getNumero2());
+        return new CalculadoraResponse(request.getNumero1(), request.getNumero2(), "subtração", resultado);
+    }
+
+    @PostMapping("/multiplicar")
+    public CalculadoraResponse multiplicar(CalculadoraRequest request) {
+        double resultado = calculadoraService.multiplicar(request.getNumero1(), request.getNumero2());
+        return new CalculadoraResponse(request.getNumero1(), request.getNumero2(), "multiplicação", resultado);
+}
+
+    @PostMapping("/dividir")
+    public CalculadoraResponse dividir(CalculadoraRequest request) {
+        double resultado = calculadoraService.dividir(request.getNumero1(), request.getNumero2());
+        return new CalculadoraResponse(request.getNumero1(), request.getNumero2(), "divisão", resultado);
     }
 
 
